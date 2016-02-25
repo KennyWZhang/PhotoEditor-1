4d631caa2715efd3a10ca02382f572c499179404
Suryanarayan Sahu <Suryanarayansahu@suryanarayans-macbook-pro.local>
2016-02-25 10:26:49 +0530

    Initial Commit

---
+++ PhotoIconEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoIconEditor
+//
+//  Created by Suryanarayan Sahu on 25/02/16.
+//  Copyright © 2016 Suryanarayan Sahu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoIconEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoIconEditor
+//
+//  Created by Suryanarayan Sahu on 25/02/16.
+//  Copyright © 2016 Suryanarayan Sahu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
